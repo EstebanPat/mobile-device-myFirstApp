6947c6db2378b34c93172594780c39de1daf2598
Esteban Patiño <esteban.pg45@gmail.com>
2023-08-18 23:24:05 -0500

    Uso de Slides y giro de la pantalla

--- src/screens/WelcomeSlide.js
+++ src/screens/WelcomeSlide.js
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { Button, Text, View } from 'react-native'
+import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native'
+import Swiper from 'react-native-swiper';
 
 const WelcomeSlide = () => {
     const navigation = useNavigation();
@@ -11,13 +12,51 @@ const WelcomeSlide = () => {
     const goToLogin = () => {
         navigation.navigate("Login")
     }
+
+    const Slide1 = () => {
+      return (
+        <View>
+          <ImageBackground source={require('./images/reloj.jpg')} style={StyleSheet.imgBackground}>
+
+          </ImageBackground>
+        </View>
+      )
+    }
+    const Slide2 = () => {
+      return (
+        <View>
+          <ImageBackground source={require("./images/aaron2.png")} style={StyleSheet.imgBackground}>
+
+          </ImageBackground>
+        </View>
+      )
+    }
+    const Slide3 = () => {
+      return (
+        <View>
+          <ImageBackground source={require('./images/bombillo.jpg')} style={StyleSheet.imgBackground}>
+            <Button title='Registro' onPress={goToRegister} /> 
+            <Button title='Inicio de Sesion' onPress={goToLogin} />
+          </ImageBackground>
+        </View>
+      )
+    }
+
   return (
-    <View>
-        <Text> Slides </Text>
-        <Button title='Registro' onPress={goToRegister} /> 
-        <Button title='Inicio de Sesion' onPress={goToLogin} />
-    </View>
+    <Swiper>
+      <Slide1 />
+      <Slide2 />
+      <Slide3 />
+    </Swiper>
+    
   )
 }
+
+const styles = StyleSheet.create({
+  imgBackground: {
+    width: '100%',
+    height: '90%'
+  }
+})
 
 export default WelcomeSlide
